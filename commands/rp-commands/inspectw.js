6d539095c2c8b5e554ef28a8817704e70a620cab
TacticsCH <alexandre.javet@hotmail.com>
2019-11-11 10:51:21 +0100

    [CHANGE] Changed readme [ADD] Added inspectw command [FIX] Fixed some code

---
+++ commands/rp-commands/inspectw.js
@@ -0,0 +1,38 @@
+const { RichEmbed } = require("discord.js");
+const utils = require('../../utils.js')
+
+module.exports = {
+  name: "inspectw",
+  aliases: ["i"],
+  category: "rp-commands",
+  description: "Inspect your weapon!",
+  usage: "~inspectw",
+  run: async (bot,message,args) => {
+    d = await utils.readData()
+    e = await utils.readWeap()
+    var uid = "a" + message.author.id
+
+
+    // Gives all info from weapons.json
+    if (d[uid].Weapon != 0) {
+      const msg1 = new RichEmbed()
+      .setTitle(`Statistiques de votre arme...`)
+      .setImage(e.table[d[uid].Weapon].Image)
+      .addField(`Nom de l'arme :`, `${e.table[d[uid].Weapon].Name}`, true)
+      .addField(`Calibre :`, `${e.table[d[uid].Weapon].Caliber}`, true)
+      .addField(`Type de l'arme :`, `${e.table[d[uid].Weapon].Type}`, true)
+      .addField(`Contenance du charger :`, `${e.table[d[uid].Weapon].Mag}`, true)
+      .addField(`Portée de l'arme :`, `${e.table[d[uid].Weapon].Range}`, true)
+      .setTimestamp()
+      .setFooter(`${e.table[d[uid].Weapon].Name}`);
+
+      message.channel.send(msg1)
+
+
+    } else if (d[uid].Weapon == 0) {
+      const msg1 = new RichEmbed()
+      .setTitle(`Vous n'avez pas d'arme!`)
+      message.channel.send(msg1)
+    }
+  }
+}
